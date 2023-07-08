d8e6c266a41273eeaf0c6923daf23bd1f8bbb29f
Sangsaka Wira <sangsakawira@gmail.com>
2023-07-09 06:32:51 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body:  const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DiceState();
+}
+
+class _DiceState extends State<DicePage> {
+
+  int limit = 5;
+  int randomNumberRight = 1;
+  int randomNumberLeft = 1;
+
+  int getRandomImage(int limit) {
+    Random random = Random();
+    int randomNumber = random.nextInt(limit) + 1;
+    return randomNumber;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(onPressed:(){
+                 setState(() {
+                   randomNumberLeft=getRandomImage(limit);
+                 });
+                },child: Image.asset('images/dice$randomNumberLeft.png')),
+              )),
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(onPressed:(){
+                  setState(() {
+                    randomNumberRight=getRandomImage(limit);
+                  });
+                },child: Image.asset('images/dice$randomNumberRight.png')),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
